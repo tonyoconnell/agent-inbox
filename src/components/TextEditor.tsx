0dd86cee08f94258a0a292102473e510742ab3ef
Mike Cann <mike.cann@gmail.com>
2025-03-19 08:12:57 +0800

    fixed blocknote using the shadcn version and the correct version numbers

--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -1,17 +1,20 @@
 import * as React from "react";
 import { useBlockNoteSync } from "@convex-dev/prosemirror-sync/blocknote";
 import "@blocknote/core/fonts/inter.css";
-import { BlockNoteView } from "@blocknote/mantine";
-import "@blocknote/mantine/style.css";
+import { BlockNoteView } from "@blocknote/shadcn";
+import "@blocknote/shadcn/style.css";
 import { api } from "../../convex/_generated/api";
 
 export const TextEditor: React.FC = () => {
-  const sync = useBlockNoteSync(api.documents, "some-id", { debug: true });
+  const sync = useBlockNoteSync(api.documents, "test", { debug: true });
+
+  if (!sync.isLoading && !sync.editor)
+    sync.create({ type: "doc", content: [] });
 
   return sync.isLoading ? (
     <p>Loading...</p>
   ) : sync.editor ? (
-    <BlockNoteView editor={sync.editor} />
+    <BlockNoteView editor={sync.editor} theme={"light"} />
   ) : (
     <button onClick={() => sync.create({ type: "doc", content: [] })}>
       Create document
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: 5173,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
