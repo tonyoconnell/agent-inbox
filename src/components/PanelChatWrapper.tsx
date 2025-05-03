1dce6f800014bfc46fef298fd1d103b50ab1bca8
Anthony O'Connell <tony@one.ie>
2025-05-03 16:18:48 +0700

    Components

--- src/components/PanelChatWrapper.tsx
+++
@@ -1,9 +0,0 @@
-import { Chat } from "./Chat";
-
-export function PanelChatWrapper() {
-    return (
-        <div className="h-[calc(100vh-2rem)] flex flex-col overflow-hidden">
-            <Chat />
-        </div>
-    );
-}
